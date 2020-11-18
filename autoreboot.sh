d89baf5f475fb3e033cbc1e34de04ca2e1564662
enzodjab <73443887+enzodjab@users.noreply.github.com>
2020-11-18 21:29:08 +0100

    Add files via upload

---
+++ autoreboot.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+while [ -z $reponse ] || [ $reponse != 'oui' ]
+do
+        echo "Lancement des serveurs..."
+	sed -i '/EST/d' welcome.txt
+	dateusa=$(zdump EST)
+	dateeu=$(zdump OUEST)
+	echo $dateusa ": Washington time" >> welcome.txt
+	echo $dateeu ": London time" >> welcome.txt
+	screen -S "drift-playground" -dm bash -c 'cd /home/enzo/acServer/drift-playground/; ./acServer >> server.log'
+	screen -S "24h-of-spa" -dm bash -c 'cd /home/enzo/acServer/24h-of-spa/; ./acServer >> server.log'
+	echo "Serveurs lancés !"
+	screen -ls
+	echo "Ils démarreront automatiquement dans 48h"
+	sleep 172800
+	killall acServer
+	echo "serveurs éteints"
+	screen -ls
+	sleep 2
+done
